f152f7aec595319860a1c3d284c0687017f2e9d8
Elizabeth Li <sincerelylibby@gmail.com>
2018-08-06 16:01:05 -0700

    Implement spiralMatrix.

--- exercises/matrix/index.js
+++ exercises/matrix/index.js
@@ -15,6 +15,45 @@
 //     [11, 16, 15, 6],
 //     [10,  9,  8, 7]]
 
-function matrix(n) {}
+function matrix(n) {
+  const result = [];
+  for (let i = 0; i < n; i += 1) {
+    result.push([]);
+  }
+
+  let number = 1;
+  let startColumn = 0;
+  let endColumn = n - 1;
+  let startRow = 0;
+  let endRow = n - 1;
+
+  while (startColumn <= endColumn && startRow <= endRow) {
+    for (let i = startColumn; i <= endColumn; i += 1) {
+      result[startRow][i] = number;
+      number += 1;
+    }
+    startRow += 1;
+
+    for (let i = startRow; i <= endRow; i += 1) {
+      result[i][endColumn] = number;
+      number += 1;
+    }
+    endColumn -= 1;
+
+    for (let i = endColumn; i >= startColumn; i -= 1) {
+      result[endRow][i] = number;
+      number += 1;
+    }
+    endRow -= 1;
+
+    for (let i = endRow; i >= startRow; i -= 1) {
+      result[i][startColumn] = number;
+      number += 1;
+    }
+    startColumn += 1;
+  }
+
+  return result;
+}
 
 module.exports = matrix;
