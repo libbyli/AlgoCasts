cff785a0b5afdf1140562c59021c9052661e520a
Elizabeth Li <sincerelylibby@gmail.com>
2018-08-07 18:02:35 -0700

    Implement maxchar.

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,28 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+  const holder = {};
+  let max = 0;
+  let result = '';
+
+  for (let char of str) {
+    if (!holder[char]) {
+      holder[char] = 1
+    } else {
+      holder[char] += 1;
+    }
+  }
+
+  const chars = Object.keys(holder);
+  for (let char of chars) {
+    if (holder[char] > max) {
+      max = holder[char];
+      result = char;
+    }
+  }
+
+  return result;
+}
 
 module.exports = maxChar;
