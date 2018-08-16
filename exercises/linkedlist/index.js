9819153e8e904976fa92c19ceee49155f43e210c
Elizabeth Li <sincerelylibby@gmail.com>
2018-08-15 20:35:08 -0700

    Work on linkedlist.

--- exercises/linkedlist/index.js
+++ exercises/linkedlist/index.js
@@ -2,8 +2,32 @@
 // Implement classes Node and Linked Lists
 // See 'directions' document
 
-class Node {}
+class Node {
+  constructor(data, next = null) {
+    this.data = data;
+    this.next = next;
+  }
+}
 
-class LinkedList {}
+class LinkedList {
+  constructor(head = null) {
+    this.head = null;
+  }
+
+  insertFirst(data) {
+    const node = new Node(data, this.head);
+    this.head = node;
+  }
+
+  size() {
+    let node = this.head;
+    let count = 0;
+    while (node) {
+      count += 1;
+      node = node.next;
+    }
+    return count;
+  }
+}
 
 module.exports = { Node, LinkedList };
