0480db08a3d85469e22b812d122bddd07b998ba4
Elizabeth Li <sincerelylibby@gmail.com>
2018-08-17 11:55:31 -0700

    Implement circular.

--- exercises/circular/index.js
+++ exercises/circular/index.js
@@ -12,6 +12,20 @@
 //   c.next = b;
 //   circular(l) // true
 
-function circular(list) {}
+function circular(list) {
+  let slowPointer = list.head;
+  let fastPointer = list.head;
+
+  while (fastPointer.next && fastPointer.next.next) {
+    slowPointer = slowPointer.next;
+    fastPointer = fastPointer.next.next;
+    
+    if (slowPointer === fastPointer) {
+      return true;
+    }
+  }
+
+  return false;
+}
 
 module.exports = circular;
